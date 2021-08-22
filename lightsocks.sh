aabeb9287f3028868dc0ef40e7ed794e7c526a51
Chen-Shiyang <88418365+Chen-Shiyang@users.noreply.github.com>
2021-08-22 11:21:56 +0800

    Update lightsocks.sh

--- lightsocks.sh
+++ lightsocks.sh
@@ -2,7 +2,6 @@
 wget https://raw.githubusercontent.com/Chen-Shiyang/scripts/main/lightsocks.go
 wget https://golang.org/dl/go1.16.7.linux-amd64.tar.gz
 tar -zxvf go1.16.7.linux-amd64.tar.gz
-./go/bin/go run 
 cat > /etc/systemd/system/lightsocks.service <<EOF
 [Unit]
 Description=lightsocks
