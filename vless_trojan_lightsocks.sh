bbb05d010dc6016818a39424016197ea5373168a
Chen-Shiyang <88418365+Chen-Shiyang@users.noreply.github.com>
2021-08-22 11:42:38 +0800

    Update and rename vless_trojan.sh to vless_trojan_lightsocks.sh

--- vless_trojan_lightsocks.sh
+++ vless_trojan_lightsocks.sh
@@ -11,6 +11,32 @@ echo net.ipv4.tcp_congestion_control=bbr >> /etc/sysctl.conf
 sysctl -p && sysctl net.ipv4.tcp_available_congestion_control
 lsmod | grep bbr
 
+# lightsocks
+wget https://raw.githubusercontent.com/Chen-Shiyang/scripts/main/lightsocks.go
+wget https://golang.org/dl/go1.16.7.linux-amd64.tar.gz
+tar -zxvf go1.16.7.linux-amd64.tar.gz
+
+cat >/etc/systemd/system/lightsocks.service<<-EOF
+[Unit]
+Description=lightsocks Service
+After=network.target nss-lookup.target
+[Service]
+User=root
+#User=nobody
+#CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+#AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/root/go/bin/go run /root/lightsocks.go
+Restart=on-failure
+RestartPreventExitStatus=23
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable lightsocks.service
+systemctl restart lightsocks.service
+
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
   sudoCmd="sudo"
 else
