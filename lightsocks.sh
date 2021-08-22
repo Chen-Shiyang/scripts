535fa5b320fc995516a09c5bdcd6634ad5e2647a
Chen-Shiyang <88418365+Chen-Shiyang@users.noreply.github.com>
2021-08-22 11:33:22 +0800

    Update lightsocks.sh

--- lightsocks.sh
+++ lightsocks.sh
@@ -2,19 +2,26 @@
 wget https://raw.githubusercontent.com/Chen-Shiyang/scripts/main/lightsocks.go
 wget https://golang.org/dl/go1.16.7.linux-amd64.tar.gz
 tar -zxvf go1.16.7.linux-amd64.tar.gz
-cat > /etc/systemd/system/lightsocks.service <<EOF
+
+cat >/etc/systemd/system/lightsocks.service<<-EOF
 [Unit]
-Description=lightsocks
-After=network.target
+Description=lightsocks Service
+After=network.target nss-lookup.target
 
 [Service]
-Type=simple
+User=root
+#User=nobody
+#CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+#AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
 ExecStart=/root/go/bin/go run /root/lightsocks.go
 Restart=on-failure
+RestartPreventExitStatus=23
 
 [Install]
 WantedBy=multi-user.target
 EOF
-systemctl start lightsocks
-systemctl enable lightsocks
-systemctl restart lightsocks
+
+systemctl daemon-reload
+systemctl enable lightsocks.service
+systemctl restart lightsocks.service
